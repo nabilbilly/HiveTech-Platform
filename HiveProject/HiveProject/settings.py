f0de70db367e6da0f7269ef577312d53cc56aef8
nabilbilly <billsonb05@gmail.com>
2024-07-22 20:03:21 -0700

    new

--- HiveProject/HiveProject/settings.py
+++ HiveProject/HiveProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'HiveProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'Template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- HiveProject/tailwind.config.js
+++ HiveProject/tailwind.config.js
@@ -1,23 +1,20 @@
 module.exports = {
-    content: [
-      './Template/**/*.html',
-      './Mainapp/**/*.html',
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-
-  module.exports = {
-    theme: {
-      extend: {
-        fontFamily: {
-          poppins: ['Poppins', 'sans-serif'],
-        },
+  content: [
+    './Template/**/*.html',
+    './Mainapp/**/*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'custom-yellow': '#FDCE02', // Add your custom color
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
+  
   
   
